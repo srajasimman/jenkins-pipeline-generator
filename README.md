355afdf9c3d32658a3ce3b018441f10e80ac2241
Rajasimman S <srajasimman@gmail.com>
2024-03-06 21:13:54 +0530

    🚦check language and build tool are matching

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ This command-line tool generates Jenkins pipeline files based on the provided pa
 
 ## Usage
 ```
-usage: jenkins-pipeline-generator.py [-h] -r REPO_URL -b BRANCH -l {java,javascript} -t {maven,gradle}
+usage: jenkins-pipeline-generator.py [-h] -r REPO_URL -b BRANCH -l {java,javascript} -t {maven,gradle,npm}
 
 Generate Jenkins pipeline file
 
@@ -12,7 +12,7 @@ options:
   -r REPO_URL,          --repo-url REPO_URL            Repository URL
   -b BRANCH,            --branch BRANCH                Branch
   -l {java,javascript}, --language {java,javascript}   Language
-  -t {maven,gradle},    --build-tool {maven,gradle}    Build tool
+  -t {maven,gradle,npm},    --build-tool {maven,gradle,npm}    Build tool
 ```
 ## Description
 This tool creates a Jenkins pipeline file tailored to your project's needs. It automates the process of setting up Jenkins pipelines for Java and JavaScript projects using either Maven or Gradle as the build tool.
@@ -22,7 +22,7 @@ This tool creates a Jenkins pipeline file tailored to your project's needs. It a
 - `-r REPO_URL, --repo-url REPO_URL`: Specifies the repository URL.
 - `-b BRANCH, --branch BRANCH`: Specifies the branch name.
 - `-l {java,javascript}, --language {java,javascript}`: Specifies the programming language (either Java or JavaScript).
-- `-t {maven,gradle}, --build-tool {maven,gradle}`: Specifies the build tool (either Maven or Gradle).
+- `-t {maven,gradle,npm}, --build-tool {maven,gradle,npm}`: Specifies the build tool (either Maven or Gradle).
 
 ## Example
 ```bash
--- jenkins-pipeline-generator.py
+++ jenkins-pipeline-generator.py
@@ -21,14 +21,26 @@ def generate_pipeline(repo_url, branch, language, build_tool):
     return pipeline_script
 
 def main():
+    options = {
+        'languages': ['java', 'javascript'],
+        'build_tools': ['maven', 'gradle', 'npm'],
+    }
+
     # Parse command-line arguments
     parser = argparse.ArgumentParser(description='Generate Jenkins pipeline file')
     parser.add_argument('-r', '--repo-url', type=str, help='Repository URL', dest='repo_url', required=True)
     parser.add_argument('-b', '--branch', type=str, help='Branch', dest='branch', required=True)
-    parser.add_argument('-l', '--language', type=str, help='Language', dest='language', required=True, choices=['java', 'javascript'])
-    parser.add_argument('-t', '--build-tool', type=str, help='Build tool', dest='build_tool', required=True, choices=['maven', 'gradle'])
+    parser.add_argument('-l', '--language', type=str, help='Language', dest='language', required=True, choices=options['languages'])
+    parser.add_argument('-t', '--build-tool', type=str, help='Build tool', dest='build_tool', required=True, choices=options['build_tools'])
     
     args = parser.parse_args()
+
+    # if args.language is 'java' and args.build_tool is not 'maven' or args.build_tool or not 'gradle'
+    if args.language == 'java' and args.build_tool != 'maven' and args.build_tool != 'gradle':
+        parser.error( "\033[91m" + args.build_tool + '\033[0m is a Invalid build tool for Java language')
+    
+    if args.language == 'javascript' and args.build_tool != 'npm':
+        parser.error( "\033[91m" + args.build_tool + '\033[0m is a Invalid build tool for Javascript language')
 
     # Generate Jenkins pipeline script
     pipeline_script = generate_pipeline(
